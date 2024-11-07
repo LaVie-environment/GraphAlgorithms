6e4b525ffb0b4f18642e488e8c431ab83b431f46
Taofeek <bellotaophyc@outlook.com>
2024-11-07 21:37:36 +0100

    solving the same problem recursively

--- depthFirstPrint.py
+++ depthFirstPrint.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
-def depth_first_print(graph, source):
-    list = [source]
-    while list:
-        current = list.pop()
-        print(current)
+# def depth_first_print(graph, source):
+#    list = [source]
+#    while list:
+#        current = list.pop()
+#        print(current)
 
-        for neighbor in graph[current]:
-            list.append(neighbor)
+#        for neighbor in graph[current]:
+#            list.append(neighbor)
+
+# graph = {
+#    'a': ['c', 'b'],
+#    'b': ['d'],
+#    'c': ['e'],
+#    'd': ['f'],
+#    'e': [],
+#    'f': []
+# }
+
+def depth_first_print(graph, source):
+    print(source)
+    for neighbor in graph[source]:
+        depth_first_print(graph, neighbor)
 
 graph = {
-    'a': ['c', 'b'],
+    'a': ['b', 'c'],
     'b': ['d'],
     'c': ['e'],
     'd': ['f'],
@@ -19,3 +33,4 @@ graph = {
 }
 
 depth_first_print(graph, 'a')
+
