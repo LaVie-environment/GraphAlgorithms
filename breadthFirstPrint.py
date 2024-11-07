fa406e3d05ee017e302478286a38105f428e237a
Taofeek <bellotaophyc@outlook.com>
2024-11-07 22:05:32 +0100

    implement the breadthFirst Algorithm

---
+++ breadthFirstPrint.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from collections import deque
+
+def breadth_first_print(graph, source):
+    queue = deque([source])
+    while queue:
+        current = queue.popleft()
+        print(current)
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+
+graph = {
+    'a': ['c', 'b'],
+    'b': ['d'],
+    'c': ['e'],
+    'd': ['f'],
+    'e': [],
+    'f': []
+}
+
+breadth_first_print(graph, 'a')
